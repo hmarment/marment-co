3e0c40d09e62e841d83aa765e2fa6ca8289acbdf
Henry Marment <henry@adsquare.com>
2019-01-16 20:44:50 +0100

    add environment variables for front-end routes update vue code to use environment variables

--- marment_io/client/src/components/HelloWorld.vue
+++ marment_io/client/src/components/HelloWorld.vue
@@ -16,7 +16,7 @@ export default {
   },
   methods: {
     getMessage() {
-      const path = 'http://localhost:8000/hello';
+      const path = `${process.env.HOST}/hello`;
       axios.get(path)
         .then((res) => {
           this.msg = res.data;
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
 from marment_io.app import hello
 
 def test_hello():
-    assert hello() == 'Hello, World!'
+    assert hello() == 'Hello, Jub!'
