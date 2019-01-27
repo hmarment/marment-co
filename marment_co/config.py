8cc68329682faa95a14ca3b00324ab1ae201fa60
Henry Marment <henry@adsquare.com>
2019-01-27 12:48:24 +0100

    update default config with explicit settings for SQLAlchemy

--- marment_co/config.py
+++ marment_co/config.py
@@ -4,6 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     DEBUG = False
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     GITHUB_API_KEY = os.environ['GITHUB_API_KEY']
 
